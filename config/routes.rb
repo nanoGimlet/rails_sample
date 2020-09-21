47f71349b73e48bc8204b9ce0d437797156b34a2
Gimlet <45482966+nanoGimlet@users.noreply.github.com>
2020-09-21 17:52:21 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,16 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
