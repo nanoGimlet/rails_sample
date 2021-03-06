9595dd2554820f06f234839dbd45091804fb08f9
Gimlet <45482966+nanoGimlet@users.noreply.github.com>
2021-03-06 10:48:23 +0900

    new

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,7 +5,7 @@ class AccountActivationsController < ApplicationController
     if !user&.activated? && user&.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "アカウントを有効化しました。"
+      flash[:success] = "アカウントを有効化しました。" # gitの練習のため
       redirect_to user
     else
       flash[:danger] = "無効なリンクです。再度取得してください。"
